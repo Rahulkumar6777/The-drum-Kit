221dc1abd69ff513f1abaf2399030595a12ac7af
rahul kumar <rahulk48546@gmail.com>
2024-07-30 19:28:42 +0530

    commit 2

--- drum-kit.js
+++ drum-kit.js
@@ -1,2 +1,26 @@
+const buttons = document.querySelectorAll("button");
+const sounds = {
+    "w": 'sounds/tom-1.mp3',
+    "a": 'sounds/tom-2.mp3',
+    "s": 'sounds/tom-3.mp3',
+    "d": 'sounds/tom-4.mp3',
+    "j": 'sounds/snare.mp3',
+    "k": 'sounds/crash.mp3',
+    "l": 'sounds/kick-bass.mp3'
+};
 
+buttons.forEach(button => {
+    button.addEventListener("click", drumClick);
+});
+
+function drumClick(event) {
+    const buttonClass = event.target.classList[0];
+    for (let rk in sounds) {
+        if (buttonClass === rk) {
+            const audio = new Audio(sounds[rk]);
+            audio.play();
+            break;
+        }
+    }
+}
 
